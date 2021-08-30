dd3500c8d7194db04d9971e4b35874d119fe2796
Ali Bulut <ali@alibulut.net>
2021-08-30 18:51:01 +0300

    coming soon gif added to homepage

--- client/src/views/Home/HomePage.js
+++ client/src/views/Home/HomePage.js
@@ -1,7 +1,12 @@
 import React from "react";
+import logo from "../../assets/img/coming-soon.gif";
 
 const HomePage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <img style={{ width: 600 }} src={logo} alt="Coming Soon..." />
+    </div>
+  );
 };
 
 export default HomePage;
