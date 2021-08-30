0cbbf7bacc3816338993c95a56466ff14a3006df
Ali Bulut <ali@alibulut.net>
2021-08-30 15:06:18 +0300

    index.js file has been updated for redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import configureStore from "./store/routing/StoreConfiguration";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import history from "./history";
+
+// bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+
+// toastify
+import "react-toastify/dist/ReactToastify.css";
+
+const store = configureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router history={history}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
