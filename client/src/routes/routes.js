0f5534b7391fabde8d88fee2d5d9d56c131bcdea
Ali Bulut <ali@alibulut.net>
2021-08-31 14:46:22 +0300

    PartiesPage added to routes

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -7,6 +7,7 @@ import { getSubDomain } from "../helpers/RouteRedirection";
 import RolesPrefix from "../constants/RolesPrefix";
 import HomePage from "../views/Home/HomePage";
 import AdminHomePage from "../views/Admin/AdminHomePage";
+import PartiesPage from "../views/Admin/Barcode/Party/PartiesPage";
 
 const isAdminSite = getSubDomain() === RolesPrefix.admin;
 
@@ -30,6 +31,22 @@ export default [
         {<Redirect to="/login" />}
       </div>
     ),
+  },
+  {
+    path: "/barcode/parties",
+    public: false,
+    exact: true,
+    roles: [RolesPrefix.admin],
+    allowedSubdomains: [RolesPrefix.admin],
+    component: () => <PartiesPage />,
+  },
+  {
+    path: "/barcode/templates",
+    public: false,
+    exact: true,
+    roles: [RolesPrefix.admin],
+    allowedSubdomains: [RolesPrefix.admin],
+    component: () => <PartiesPage />,
   },
   {
     path: "/",
