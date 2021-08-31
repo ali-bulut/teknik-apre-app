d498c890caf25aa9b288ad8d54fc68e007b704b8
Ali Bulut <ali@alibulut.net>
2021-08-31 14:43:47 +0300

    updating navbar design

--- client/src/components/Common/Navbar/AdminNavbar.js
+++ client/src/components/Common/Navbar/AdminNavbar.js
@@ -1,7 +1,53 @@
 import React from "react";
+import { Nav, NavDropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const AdminNavbar = (props) => {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <React.Fragment>
+      <NavDropdown
+        className={
+          (props.pathName === "/barcode/parties" ||
+            props.pathName === "/barcode/templates") &&
+          "navbar-link-container-active"
+        }
+        title={
+          <div className="navbar-link-container">
+            <i className="material-icons navbar-icon">qr_code_scanner</i>
+            <span className="main-navbar-link">Barkod</span>
+          </div>
+        }
+        id="collasible-nav-dropdown"
+      >
+        <NavDropdown.Item
+          className={
+            props.pathName === "/barcode/parties" &&
+            "navbar-link-container-active"
+          }
+          as={Link}
+          to="/barcode/parties"
+        >
+          <div className="navbar-link-container">
+            <i className="material-icons navbar-icon">article</i>
+            <span className="main-navbar-link">Partiler</span>
+          </div>
+        </NavDropdown.Item>
+        <NavDropdown.Item
+          className={
+            props.pathName === "/barcode/templates" &&
+            "navbar-link-container-active"
+          }
+          as={Link}
+          to="/barcode/templates"
+        >
+          <div className="navbar-link-container">
+            <i className="material-icons navbar-icon">view_compact</i>
+            <span className="main-navbar-link">Şablonlar</span>
+          </div>
+        </NavDropdown.Item>
+      </NavDropdown>
+    </React.Fragment>
+  );
 };
 
 export default AdminNavbar;
