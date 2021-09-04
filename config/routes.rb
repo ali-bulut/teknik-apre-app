290a5ec7e1fb01723cadeeb8a72b9b33e93346ac
Ali Bulut <ali@alibulut.net>
2021-09-04 18:30:39 +0300

    template related scaffold files added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  resources :templates
   mount_devise_token_auth_for 'User', at: 'auth', controllers: {
     registrations: 'users/registrations'
   }
-  get '/books', to: 'books#show'
 end
