c699a7b6491743dd29d1f180246f44f4e4d80984
Ali Bulut <ali@alibulut.net>
2021-09-09 23:53:37 +0300

    cors_preflight check updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::API
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, ' \
-      'Auth-Token, Email, X-User-Token, X-User-Email, x-xsrf-token, access-token, uid, client, expiry'
+      'Auth-Token, Email, X-User-Token, X-User-Email, x-xsrf-token, access-token, uid, client, expiry, access-control-allow-origin'
     response.headers['Access-Control-Max-Age'] = '1728000'
     response.headers['Access-Control-Allow-Credentials'] = true
   end
