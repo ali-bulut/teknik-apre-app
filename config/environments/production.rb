e1dcff32b688e5f52e5ab2116419eca4f2868009
Ali Bulut <ali@alibulut.net>
2021-09-09 23:44:36 +0300

    updating config.hosts option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.hosts << ENV["API_URL"]
-  config.hosts << ENV["BASE_DOMAIN"]
-  config.hosts << ENV["CLOUDFRONT_URL"]
+  config.hosts.clear
 
   # Code is not reloaded between requests.
   config.cache_classes = true
