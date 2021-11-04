bb1f5dd56c8c233fbbd612447c9da788e80217fb
Ali Bulut <ali@alibulut.net>
2021-11-04 11:30:36 +0300

    unused methods removed from template_value model

--- app/models/template_value.rb
+++ app/models/template_value.rb
@@ -6,14 +6,9 @@ class TemplateValue < ApplicationRecord
     net_kg: "net kg",
     net_mt: "net mt",
     gross_mt: "gross mt",
-    width: "en"
   }
 
-  scope :gross_kg?, -> { where(column_name: TemplateValue.calculated_value_types[:gross_kg]) }
-  scope :net_kg?, -> { where(column_name: TemplateValue.calculated_value_types[:net_kg]) }
-  scope :gross_mt?, -> { where(column_name: TemplateValue.calculated_value_types[:gross_mt]) }
   scope :net_mt?, -> { where(column_name: TemplateValue.calculated_value_types[:net_mt]) }
-  scope :width?, -> { where(column_name: TemplateValue.calculated_value_types[:width]) }
 
   def gross_kg?
     self.column_name == TemplateValue.calculated_value_types[:gross_kg]
@@ -25,14 +20,6 @@ class TemplateValue < ApplicationRecord
 
   def gross_mt?
     self.column_name == TemplateValue.calculated_value_types[:gross_mt]
-  end
-
-  def net_mt?
-    self.column_name == TemplateValue.calculated_value_types[:net_mt]
-  end
-
-  def width?
-    self.column_name == TemplateValue.calculated_value_types[:width]
   end
 
 end
